1d53a9c2152f172e875fdedeb043478ce2e98d46
aiden2978 <aiden2978@gmail.com>
2023-11-27 00:10:13 +0900

    231127

--- class 3/2630.py
+++ class 3/2630.py
@@ -1,9 +1,6 @@
 import sys
-import math
-import numpy as np
 
 N = int(sys.stdin.readline())
-n = int(math.log2(N))
 
 square = []
 for _ in range(N):
@@ -11,15 +8,20 @@ for _ in range(N):
 
 
 cnt_white = 0
-def papers_white(list, r, c, d):
-    divided_list = np.array(list)[r:r+d, c:c+d].tolist()
-    print(divided_list)
-    if len(divided_list) == 1:
-        if divided_list == [0]:
-            return 1
-    elif set(sum(divided_list, [])) == {0}:
-        return 1
+cnt_blue = 0
+def papers(list, r, c, d):
+    global cnt_white, cnt_blue
+    divided_list = [row[r:r+d] for row in list[c:c+d]]
+    if set(sum(divided_list, [])) == {0}:
+        cnt_white += 1
+    elif set(sum(divided_list, [])) == {1}:
+        cnt_blue += 1
     else:
-        return papers_white(list, r, c, int(d/2)) + papers_white(list, r + int(d/2), c, int(d/2)) + papers_white(list, r, c + int(d/2), int(d/2)) + papers_white(list, r + int(d/2), c + int(d/2), int(d/2))
+        papers(list, r, c, int(d/2))
+        papers(list, r + int(d/2), c, int(d/2))
+        papers(list, r, c + int(d/2), int(d/2))
+        papers(list, r + int(d/2), c + int(d/2), int(d/2))
     
-print(papers_white(square, 0, 0, N))
+papers(square, 0, 0, N)
+print(cnt_white)
+print(cnt_blue)

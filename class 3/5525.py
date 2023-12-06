f5e36aeb2978ead4b51c93fd67207d8c4abbc862
aiden2978 <aiden2978@gmail.com>
2023-12-06 19:35:08 +0900

    231206

--- class 3/5525.py
+++ class 3/5525.py
@@ -4,13 +4,14 @@ N = int(sys.stdin.readline())
 M = int(sys.stdin.readline())
 S = sys.stdin.readline().strip()
 
+alt = [0 for _ in range(M)]
 cnt = 0
-PN = 'I'
-for _ in range(N):
-    PN += 'OI'
-
-for i in range(M-2*N):
-    if S[i:i+(2*N+1)] == PN:
+for i in range(1, M):
+    if S[i] != S[i-1]:
+        alt[i] = alt[i-1] + 1
+    else:
+        alt[i] = 0
+    if i >= 2*N and S[i - 2*N] == "I" and alt[i] >= 2*N:
         cnt += 1
 
 print(cnt)

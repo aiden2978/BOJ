b525c35954475a4aae28cd0a03207396380b600d
aiden2978 <aiden2978@gmail.com>
2023-12-12 21:08:15 +0900

    231212

--- class 4/10830.py
+++ class 4/10830.py
@@ -4,6 +4,8 @@ N, B = map(int, sys.stdin.readline().split( ))
 mtx = []
 for _ in range(N):
     lines = list(map(int, sys.stdin.readline().split( )))
+    for i in range(N):
+        lines[i] = lines[i] % 1000
     mtx.append(lines)
 
 def mul(mtx1, mtx2):
@@ -13,13 +15,14 @@ def mul(mtx1, mtx2):
             sum = 0
             for k in range(N):
                 sum += (mtx1[i][k] * mtx2[k][j])
-                res[i][j] = sum % 1000
+            res[i][j] = sum % 1000
     return res
 
 def power(mtx, B):
     if B == 1:
         return mtx
-    res = mul(power(mtx, B // 2), power(mtx, B // 2))
+    a = power(mtx, B // 2)
+    res = mul(a, a)
     if B % 2 == 1:
         res = mul(mtx, res)
     return res

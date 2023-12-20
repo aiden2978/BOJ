fb41f563046c2758c5ba5ada16828f56538669b1
aiden2978 <aiden2978@gmail.com>
2023-12-20 22:54:55 +0900

    231220

--- class 4/9663.py
+++ class 4/9663.py
@@ -1,38 +1,34 @@
 import sys
-import itertools
+    
+def dfs():
+    global isAble, cnt
+    if len(s) == N:
+        cnt += 1
+        return
 
-def atk(q1, q2):
-    if abs(q1[0] - q2[0]) == abs(q1[1] - q2[1]):
-        return True
-    else:
-        return False
+    for i in range(N):
+        if i not in s:
+            if len(s) == 0:
+                s.append(i)
+                dfs()
+                s.pop()
+            else:
+                isAble = True
+                for j in range(len(s)):
+                    if abs(len(s) - j) == abs(i - s[j]):
+                        isAble = False
+                        break
+                if isAble:
+                    s.append(i)
+                    dfs()
+                    s.pop()
 
 N = int(sys.stdin.readline())
 
-nums = [i for i in range(N)]
-combs = list(itertools.combinations(nums, 2))
 cnt = 0
 isAble = True
 
 s = []
-
-def dfs():
-    global isAble, cnt
-    if len(s) == N:
-        isAble = True
-        for comb in combs:
-            if atk([comb[0], s[comb[0]]], [comb[1], s[comb[1]]]):
-                isAble = False
-                break
-        if isAble:
-            cnt += 1
-        return
-    
-    for i in range(N):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
 
 dfs()
 print(cnt)
